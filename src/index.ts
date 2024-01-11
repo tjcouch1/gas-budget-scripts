647b4b9250b23736e9bb5b903e07bdd31852326b
TJ Couch <tjcouch1@gmail.com>
2024-01-10 22:05:38 -0600

    Changed menu to be data-driven to prepare for putting menu in the spreadsheet

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,47 @@
 /// <reference path="./models/thread-info.model.ts" />
 /// <reference path="./models/thread-list.model.ts" />
 
+/** Menu to add to the spreadsheet and to show in the menu bar on desktop */
+const menu: {
+  [menuName: string]: ({ name: string; functionName: string } | undefined)[];
+} = {
+  Budgeting: [
+    {
+      name: "Import 50 Receipts (No mark)",
+      functionName: "getAndRecordSomeReceiptsNoMark",
+    },
+    {
+      name: "Import All Receipts (No mark)",
+      functionName: "getAndRecordReceiptsNoMark",
+    },
+    undefined,
+    {
+      name: "Import 25 Receipts and mark",
+      functionName: "getAndRecordSomeReceiptsAndMark",
+    },
+    {
+      name: "Import All Receipts and mark",
+      functionName: "getAndRecordReceiptsAndMark",
+    },
+  ],
+};
+
 function onOpen() {
+  // Set up menu bar items (desktop only)
   // Menu only works on desktop :(
   // Links for potential workarounds:
   // https://stackoverflow.com/questions/77385083/custom-menu-for-mobile-version-of-google-sheet
   // https://stackoverflow.com/questions/57840757/button-click-is-only-working-on-windows-not-working-on-android-mobile-sheet
   // https://webapps.stackexchange.com/questions/87346/add-a-script-trigger-to-google-sheet-that-will-work-in-android-mobile-app
   const ui = SpreadsheetApp.getUi();
-  ui.createMenu("Budgeting")
-    .addItem("Import 50 Receipts (No mark)", "getAndRecordSomeReceiptsNoMark")
-    .addItem("Import All Receipts (No mark)", "getAndRecordReceiptsNoMark")
-    .addSeparator()
-    .addItem("Import 25 Receipts and mark", "getAndRecordSomeReceiptsAndMark")
-    .addItem("Import All Receipts and mark", "getAndRecordReceiptsAndMark")
-    .addToUi();
+  Object.entries(menu).forEach(([menuName, menuEntries]) => {
+    const uiMenu = ui.createMenu(menuName);
+    menuEntries.forEach((menuEntry) => {
+      if (menuEntry) uiMenu.addItem(menuEntry.name, menuEntry.functionName);
+      else uiMenu.addSeparator();
+    });
+    uiMenu.addToUi();
+  });
 }
 
 function getAndRecordSomeReceiptsNoMark() {
